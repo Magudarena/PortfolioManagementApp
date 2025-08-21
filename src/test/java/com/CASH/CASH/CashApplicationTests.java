75f774d3416c27d8d9c853aa1bd531a37258517a
Meg <a@a.com>
2025-08-21 14:48:14 +0000

    Starter

---
+++ src/test/java/com/CASH/CASH/CashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CASH.CASH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
