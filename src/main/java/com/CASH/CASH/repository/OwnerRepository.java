cc2213a5d94d53ab084ea5809193c1e2d0e60d6d
Meg <a@a.com>
2025-08-22 08:10:53 +0000

    implement owner and add dependencies

--- src/main/java/com/CASH/CASH/repository/OwnerRepository.java
+++ src/main/java/com/CASH/CASH/repository/OwnerRepository.java
@@ -1,4 +1,7 @@
 package com.CASH.CASH.repository;
 
-public class OwnerRepository {
+import com.CASH.CASH.model.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
 }
